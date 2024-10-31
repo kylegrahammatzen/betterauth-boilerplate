f4d7547ba0f653c1206ef3b47769187519ce6b50
Kyle <kgrahammatzen@gmail.com>
2024-10-31 02:21:28 -0400

    Next 15 change

--- app/loading.tsx
+++
@@ -1,3 +0,0 @@
-export default function Loading() {
-  return <h1>Home page loading...</h1>;
-}
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignInButton } from "@/components/SignInButton";
-
-export default async function Home() {
-  return (
-    <div>
-      <SignInButton provider="google" />
-    </div>
-  );
-}
