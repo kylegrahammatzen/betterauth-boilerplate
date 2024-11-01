69bc84e91b6d84a4de0dbfdd1171bffead850639
Kyle <kgrahammatzen@gmail.com>
2024-11-01 11:18:39 -0400

    Updated content list ui to better reflect collapsed, mobile, and full view view

--- components/ContentList.tsx
+++ components/ContentList.tsx
@@ -1,58 +1,68 @@
-"use client"
+"use client";
 
+import React from "react";
 import {
-    SidebarContent,
-    SidebarFooter,
-    SidebarGroup,
-    SidebarGroupAction,
-    SidebarHeader,
-    SidebarMenu,
-    SidebarMenuButton,
-    SidebarMenuItem,
-    useSidebar,
-  } from "@/components/ui/sidebar";
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupAction,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from "@/components/ui/sidebar";
 import {
-    Tooltip,
-    TooltipContent,
-    TooltipTrigger,
-  } from "@/components/ui/tooltip";
-import { Button } from "./ui/button";
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
+import { useIsMobile } from "@/hooks/use-mobile";
 
-const ContentList = () => {
-  const { state = "open" } = useSidebar()
+export function ContentList() {
+  const { state = "open" } = useSidebar();
+  const isMobile = useIsMobile();
+
+  const isCollapsed = state === "collapsed";
 
   return (
     <SidebarGroup>
       <SidebarMenu>
         <SidebarMenuItem>
-          <SidebarMenuButton asChild>
-            {state === "collapsed" ? (
+          {isCollapsed ? (
+            <SidebarMenuButton asChild>
               <Button variant="outline">
                 <Plus className="h-4 w-4" />
                 <span className="sr-only">Add Content</span>
               </Button>
-            ) : (
-              <span className="font-medium">Example Content</span>
-            )}
-          </SidebarMenuButton>
+            </SidebarMenuButton>
+          ) : (
+            <div className="flex justify-between items-center">
+              <span className="font-medium pl-1">Example Content</span>
+              {isMobile ? (
+                <Button size="sm" variant="link">
+                  <Plus className="h-4 w-4" />
+                  <span className="sr-only">Add Content</span>
+                </Button>
+              ) : (
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Button size="sm" variant="link">
+                      <Plus className="h-4 w-4" />
+                      <span className="sr-only">Add Content</span>
+                    </Button>
+                  </TooltipTrigger>
+                  <TooltipContent side="right" align="center">
+                    Add Content
+                  </TooltipContent>
+                </Tooltip>
+              )}
+            </div>
+          )}
         </SidebarMenuItem>
       </SidebarMenu>
-      {state !== "collapsed" && (
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <SidebarGroupAction>
-              <Plus className="h-4 w-4" />
-              <span className="sr-only">Add Content</span>
-            </SidebarGroupAction>
-          </TooltipTrigger>
-          <TooltipContent side="right" align="center">
-            Add Content
-          </TooltipContent>
-        </Tooltip>
-      )}
     </SidebarGroup>
-  )
+  );
 }
-
-export { ContentList }
